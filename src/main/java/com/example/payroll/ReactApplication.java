41ff1133adc9aceb01039cf7b4a28e62d6853ce4
youngvform <austinhbk@naver.com>
2020-05-17 15:27:12 +0900

    initial commit

---
+++ src/main/java/com/example/payroll/ReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactApplication.class, args);
+    }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/main/js/app.js",
+  devtool: "sourcemaps",
+  cache: true,
+  mode: "development",
+  output: {
+    path: path.join(__dirname, "./src/main/resources/static/built/"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: path.join(__dirname, "."),
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
